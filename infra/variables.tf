63de8d93604a41c1a8bdb2ad46a69b6f50d2974d
SeongCheol Jung <kir5121@naver.com>
2025-04-21 08:48:05 +0900

    [feat] infra파일 제정립

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "lldj123414"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "jhs512"
+}
